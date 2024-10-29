0b4b3cc8555b967582398fc103b919b9e4ec2593
Saurabh Kumar <1355saurabh@gmail.com>
2024-10-29 23:15:15 +0530

    to fetch Rating data from User service.

--- PRACTICE/MODULES/AdvancedJava/DailyPractice/Oct2024MicroServicesDurgesh/UserService/src/main/java/com/lcwd/user/service/impl/UserServiceImpl.java
+++ PRACTICE/MODULES/AdvancedJava/DailyPractice/Oct2024MicroServicesDurgesh/UserService/src/main/java/com/lcwd/user/service/impl/UserServiceImpl.java
@@ -1,11 +1,16 @@
 package com.lcwd.user.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import com.lcwd.user.service.entity.Rating;
 import com.lcwd.user.service.entity.User;
 import com.lcwd.user.service.exception.ResourceNotFoundException;
 import com.lcwd.user.service.repository.UserRepository;
@@ -16,6 +21,12 @@ public class UserServiceImpl implements UserService {
 
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	
+	private Logger logger = LoggerFactory.getLogger(UserServiceImpl.class) ;
 	
 	@Override
 	public User saveUser(User user) {
@@ -31,10 +42,23 @@ public class UserServiceImpl implements UserService {
 		return userRepository.findAll();
 	}
 
+//	@Override
+//	public User getUser(String userId) {
+//		// TODO Auto-generated method stub
+//		return userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User with given id is not found on server!"));
+//	}
+	
 	@Override
 	public User getUser(String userId) {
 		// TODO Auto-generated method stub
-		return userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User with given id is not found on server!"));
+//		get user from database with the help of user repository
+		User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User with given id is not found on server!"));
+//		fetch the rating of the above user from rating service
+//		http://localhost:8083/ratings/users/ABC
+		ArrayList<Rating> ratingsOfUser = restTemplate.getForObject("http://localhost:8083/ratings/users/b90b3e10-a40c-4837-88af-ee9c18721000", ArrayList.class);
+		logger.info("{} ", ratingsOfUser);
+		user.setRatings(ratingsOfUser);
+		return user;
 	}
 	
 }
