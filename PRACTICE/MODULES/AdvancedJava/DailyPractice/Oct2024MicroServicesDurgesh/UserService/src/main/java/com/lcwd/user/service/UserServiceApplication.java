ada275dc787d67bec2ba57fd86eb38f6986be608
Saurabh Kumar <1355saurabh@gmail.com>
2024-10-29 20:58:45 +0530

    User service connected with Service Registry

--- PRACTICE/MODULES/AdvancedJava/DailyPractice/Oct2024MicroServicesDurgesh/UserService/src/main/java/com/lcwd/user/service/UserServiceApplication.java
+++ PRACTICE/MODULES/AdvancedJava/DailyPractice/Oct2024MicroServicesDurgesh/UserService/src/main/java/com/lcwd/user/service/UserServiceApplication.java
@@ -2,8 +2,10 @@ package com.lcwd.user.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient	// this provide clearity to the code that this is a client which is discoverable (OPTIONAL).
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
