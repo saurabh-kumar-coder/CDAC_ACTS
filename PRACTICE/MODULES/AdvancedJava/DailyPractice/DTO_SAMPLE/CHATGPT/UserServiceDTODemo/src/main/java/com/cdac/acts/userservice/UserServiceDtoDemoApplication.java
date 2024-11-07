cae3b2e0dc43da0fecb2faca0c381d06660f41ef
Saurabh Kumar <1355saurabh@gmail.com>
2024-11-07 07:29:05 +0530

    DTO demo started.

---
+++ PRACTICE/MODULES/AdvancedJava/DailyPractice/DTO_SAMPLE/CHATGPT/UserServiceDTODemo/src/main/java/com/cdac/acts/userservice/UserServiceDtoDemoApplication.java
@@ -0,0 +1,13 @@
+package com.cdac.acts.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceDtoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceDtoDemoApplication.class, args);
+	}
+
+}
