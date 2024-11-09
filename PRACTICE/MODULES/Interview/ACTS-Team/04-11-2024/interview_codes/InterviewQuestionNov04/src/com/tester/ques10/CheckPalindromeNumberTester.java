3e05cf2313379dfa4cd76fb3ce9053216df0f9e8
Saurabh Kumar <1355saurabh@gmail.com>
2024-11-09 22:57:58 +0530

    palindrome number

--- PRACTICE/MODULES/Interview/ACTS-Team/04-11-2024/interview_codes/InterviewQuestionNov04/src/com/tester/ques10/CheckPalindromeNumberTester.java
+++ PRACTICE/MODULES/Interview/ACTS-Team/04-11-2024/interview_codes/InterviewQuestionNov04/src/com/tester/ques10/CheckPalindromeNumberTester.java
@@ -1,7 +1,7 @@
 package com.tester.ques10;
 
 import com.question.ques10.CheckPalindromeNumber;
-
+//10.	Write a program to check palindrome number?
 public class CheckPalindromeNumberTester {
 	public static void main(String[] args) {
 		int num = 1551;
