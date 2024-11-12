126ce287aaf2f1d61baaffa78cea9b0d4ad781f1
Saurabh Kumar <1355saurabh@gmail.com>
2024-11-12 16:42:08 +0530

    UserDetailsService inteface implemented on UserDetailsImpl class to find the user using JPA REPOSITORY,
    #add @service annotation on the class.

---
+++ PRACTICE/MODULES/AdvancedJava/DailyPractice/LearnWithIfte_JWT/SpringJWT/src/main/java/com/youtube/service/UserDetailsImpl.java
@@ -0,0 +1,23 @@
+package com.youtube.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.youtube.repository.UserRepository;
+
+@Service
+public class UserDetailsImpl implements UserDetailsService {
+
+	@Autowired
+	private UserRepository userRepository;
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO Auto-generated method stub
+		return userRepository.findByUserName(username)
+				.orElseThrow(() -> new UsernameNotFoundException("User not found"));
+	}
+
+}
