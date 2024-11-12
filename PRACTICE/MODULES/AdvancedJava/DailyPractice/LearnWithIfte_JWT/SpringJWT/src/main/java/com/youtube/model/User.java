d75fa8e1cbc871397440060dbc12278498e3ff9d
Saurabh Kumar <1355saurabh@gmail.com>
2024-11-12 16:17:13 +0530

    Created user entity with attributes.
    UserDetails interface implemented in User for authentication and authorization.
    
    created a JPA repository with a self build method findByUserName();

---
+++ PRACTICE/MODULES/AdvancedJava/DailyPractice/LearnWithIfte_JWT/SpringJWT/src/main/java/com/youtube/model/User.java
@@ -0,0 +1,81 @@
+package com.youtube.model;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "user")
+public class User implements UserDetails {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Integer id;
+	
+	@Column(name = "first_name")
+	private String firstName;
+	
+	@Column(name = "last_name")
+	private String lastName;
+	
+	@Column(name = "username")
+	private String userName;
+	
+	@Column(name = "password")
+	private String password;
+	
+	@Enumerated(value = EnumType.STRING)
+	private Role role;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return List.of(new SimpleGrantedAuthority(role.name()));
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	
+}
---
+++ PRACTICE/MODULES/AdvancedJava/DailyPractice/LearnWithIfte_JWT/SpringJWT/src/main/java/com/youtube/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.youtube.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.youtube.model.User;
+
+public interface UserRepository extends JpaRepository<User, Integer>{
+	Optional<User> findByUserName(String username);
+}
