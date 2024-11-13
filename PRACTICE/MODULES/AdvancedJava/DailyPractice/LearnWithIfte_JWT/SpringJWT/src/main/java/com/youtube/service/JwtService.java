b87c02daaa72ed08b171eea4a63597dbb04e659e
Saurabh Kumar <1355saurabh@gmail.com>
2024-11-13 20:53:16 +0530

    service and filter created for JWT.

--- PRACTICE/MODULES/AdvancedJava/DailyPractice/LearnWithIfte_JWT/SpringJWT/src/main/java/com/youtube/service/JwtService.java
+++ PRACTICE/MODULES/AdvancedJava/DailyPractice/LearnWithIfte_JWT/SpringJWT/src/main/java/com/youtube/service/JwtService.java
@@ -1,13 +1,16 @@
 package com.youtube.service;
 
 import java.util.Date;
+import java.util.function.Function;
 
 import javax.crypto.SecretKey;
 
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import com.youtube.model.User;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
@@ -33,5 +36,38 @@ public class JwtService {
 		// TODO Auto-generated method stub
 		byte[] keyBytes = Decoders.BASE64URL.decode(SECRET_KEY);
 		return Keys.hmacShaKeyFor(keyBytes);
+	}
+	
+	private Claims extractAllClaims(String token) {
+		return Jwts
+				.parser()
+				.verifyWith(getSigninKey())
+				.build()
+				.parseSignedClaims(token)
+				.getPayload();
+	}
+	
+	public <T> T extractClaim(String token, Function<Claims, T> resolver) {
+		Claims claims = extractAllClaims(token);
+		return resolver.apply(claims);
+	}
+	
+	public String extractUsername(String token) {
+		return extractClaim(token, Claims::getSubject);
+	}
+	
+	public boolean isValid(String token, UserDetails user) {
+		String username = extractUsername(token);
+		return (username.equals(user.getUsername())) && isTokenExpired(token);
+	}
+
+	private boolean isTokenExpired(String token) {
+		// TODO Auto-generated method stub
+		return extractExpiration(token).before(new Date());
+	}
+
+	private Date extractExpiration(String token) {
+		// TODO Auto-generated method stub
+		return extractClaim(token, Claims::getExpiration);
 	}
 }
