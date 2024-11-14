9a96b900d4cbc0a88a17e736e10280f118c38014
Saurabh Kumar <1355saurabh@gmail.com>
2024-11-14 18:38:46 +0530

    JwtAuthenticationFilter file added.

--- PRACTICE/MODULES/AdvancedJava/DailyPractice/LearnWithIfte_JWT/SpringJWT/src/main/java/com/youtube/filter/JwtAuthenticationFilter.java
+++ PRACTICE/MODULES/AdvancedJava/DailyPractice/LearnWithIfte_JWT/SpringJWT/src/main/java/com/youtube/filter/JwtAuthenticationFilter.java
@@ -6,7 +6,6 @@ import org.antlr.v4.runtime.misc.NotNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.filter.OncePerRequestFilter;
 
