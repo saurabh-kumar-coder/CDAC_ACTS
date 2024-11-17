bc1ef6dcbd316ec599bd6e5e88947c578590f084
Saurabh Kumar <1355saurabh@gmail.com>
2024-11-17 06:36:16 +0530

    done with JWT.

--- PRACTICE/MODULES/AdvancedJava/DailyPractice/LearnWithIfte_JWT/SpringJWT/src/main/java/com/youtube/config/SecurityConfig.java
+++ PRACTICE/MODULES/AdvancedJava/DailyPractice/LearnWithIfte_JWT/SpringJWT/src/main/java/com/youtube/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.youtube.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,6 +12,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.youtube.filter.JwtAuthenticationFilter;
@@ -24,13 +26,16 @@ public class SecurityConfig {
 
 	private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
-	public SecurityConfig(UserDetailsServiceImp userDetailsServiceImp,
-			JwtAuthenticationFilter jwtAuthenticationFilter) {
+	private final CustomAccessDeniedHandler customAccessDeniedHandler;
+	
+	public SecurityConfig(UserDetailsServiceImp userDetailsServiceImp, JwtAuthenticationFilter jwtAuthenticationFilter,
+			CustomAccessDeniedHandler customAccessDeniedHandler) {
 		super();
 		this.userDetailsServiceImp = userDetailsServiceImp;
 		this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+		this.customAccessDeniedHandler = customAccessDeniedHandler;
 	}
-	
+
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		return http
@@ -41,6 +46,8 @@ public class SecurityConfig {
 												.anyRequest()
 												.authenticated()
 												).userDetailsService(userDetailsServiceImp)
+				.exceptionHandling(e -> e.accessDeniedHandler(customAccessDeniedHandler)
+						.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
 				.sessionManagement(
 						session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
