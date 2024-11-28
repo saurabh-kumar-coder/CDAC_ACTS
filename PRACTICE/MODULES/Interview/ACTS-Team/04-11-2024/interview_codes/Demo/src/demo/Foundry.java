c2ce15e8d591d0a83ebb527577faa3beb5174fab
Saurabh Kumar <1355saurabh@gmail.com>
2024-11-28 21:36:01 +0530

    throws exception in constructor.

---
+++ PRACTICE/MODULES/Interview/ACTS-Team/04-11-2024/interview_codes/Demo/src/demo/Foundry.java
@@ -0,0 +1,24 @@
+package demo;
+
+import java.io.IOException;
+
+class San
+{
+     San()throws IOException
+     {
+  
+     } 
+ 
+}
+public class Foundry extends San
+{
+     Foundry() throws IOException
+     {
+
+     }
+     public static void main(String[]args)
+     {
+  
+     }
+}
+
