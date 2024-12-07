a087a39a1e1e0f642891ffd9339d4544bc0ebb7d
Saurabh Kumar <1355saurabh@gmail.com>
2024-12-07 19:30:01 +0530

    updated.

--- PRACTICE/MODULES/WebTechonolgy/day14/RoutingDemo/src/App.jsx
+++ PRACTICE/MODULES/WebTechonolgy/day14/RoutingDemo/src/App.jsx
@@ -1,17 +1,15 @@
-
-import UseEffect from './components/useEffectDemo/UseEffectDemo'
-import Outer from './components/ChildToParent/Outer'
+import UseEffect from "./components/useEffectDemo/UseEffectDemo";
+import Outer from "./components/ChildToParent/Outer";
 
 function App() {
-
-  return (
-    <>
-      <div>Hello</div>
-      <div>Hekkiii</div>
-      {/* <UseEffect /> */}
-      <Outer />
-    </>
-  )
+	return (
+		<>
+			<div>Hello</div>
+			<div>Hekkiii</div>
+			<UseEffect />
+			<Outer />
+		</>
+	);
 }
 
-export default App
+export default App;
