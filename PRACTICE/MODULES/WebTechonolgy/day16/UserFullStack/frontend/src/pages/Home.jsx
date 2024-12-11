8b2d53e12247b93fec2bedc9e62b4fad60fd1b3b
Saurabh Kumar <1355saurabh@gmail.com>
2024-12-11 21:09:19 +0530

    api call using axios also done.

--- PRACTICE/MODULES/WebTechonolgy/day16/UserFullStack/frontend/src/pages/Home.jsx
+++ PRACTICE/MODULES/WebTechonolgy/day16/UserFullStack/frontend/src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 import { Button, Col, Row, Table } from "react-bootstrap";
 
@@ -6,49 +7,120 @@ const Home = () => {
 	const [uname, setUname] = useState("");
 	const [city, setCity] = useState("");
 	const [address, setAddress] = useState("");
-	const [updateOnPost, setUpdateOnPost] = useState(false);
+	// const [updateOnPost, setUpdateOnPost] = useState(false);
+	const [flagToGetData, setFlagToGetData] = useState(false);
+	// useEffect(() => {
+	// 	const getUserData = async () => {
+	// 		try {
+	// 			setUpdateOnPost(false);
+	// 			let response = await fetch("http://localhost:3000/user");
+	// 			if (!response.ok) throw new Error("Not Fetched");
+	// 			const result = await response.json();
+
+	// 			setData(result);
+	// 			console.log(result);
+	// 		} catch (error) {
+	// 			console.log(error);
+	// 		}
+	// 	};
+	// 	getUserData();
+	// }, [updateOnPost]);
+
 	useEffect(() => {
 		const getUserData = async () => {
 			try {
-				setUpdateOnPost(false);
-				let response = await fetch("http://localhost:3000/user");
-				if (!response.ok) throw new Error("Not Fetched");
-				const result = await response.json();
-
+				setFlagToGetData(false);
+				let response = await axios.get("http://localhost:3000/user");
+				if (!response) throw new Error("Data Not Fetched...");
+				const result = response.data;
 				setData(result);
-				console.log(result);
 			} catch (error) {
 				console.log(error);
 			}
 		};
 		getUserData();
-	}, [updateOnPost]);
+	}, [flagToGetData]);
+
+	// const addUser = () => {
+	// 	let newUser = { uname: uname, city: city, address: address };
+	// 	console.log(newUser);
+	// 	const addUser = async () => {
+	// 		try {
+	// 			const response = await fetch("http://localhost:3000/user", {
+	// 				method: "POST",
+	// 				headers: {
+	// 					"Content-Type": "application/json",
+	// 				},
+	// 				body: JSON.stringify(newUser),
+	// 			});
+
+	// 			if (!response) {
+	// 				throw new Error("Unable to add data.");
+	// 			}
+	// 			const result = await response.json();
+	// 			console.log(result);
+	// 			setUpdateOnPost(true);
+	// 		} catch (error) {
+	// 			console.log(error);
+	// 		}
+	// 	};
+
+	// 	addUser();
+	// 	clearInputField();
+	// };
 
 	const addUser = () => {
-		let newUser = { uname: uname, city: city, address: address };
-		console.log(newUser);
+		const newUser = { uname: uname, city: city, address: address };
 		const addUser = async () => {
 			try {
-				const response = await fetch("http://localhost:3000/user", {
-					method: "POST",
-					headers: {
-						"Content-Type": "application/json",
-					},
-					body: JSON.stringify(newUser),
-				});
-
-				if (!response) {
-					throw new Error("Unable to add data.");
+				const response = await axios.post(
+					"http://localhost:3000/user",
+					newUser,
+					{
+						headers: {
+							"Content-Type": "application/json",
+						},
+					}
+				);
+				if (response.status === 200 || response.status === 201) {
+					setFlagToGetData(true);
+				} else {
+					clearInputField();
+					throw new Error("Unable to add data");
 				}
-				const result = await response.json();
-				console.log(result);
-				setUpdateOnPost(true);
 			} catch (error) {
 				console.log(error);
 			}
 		};
-
+		setFlagToGetData(true);
 		addUser();
+		clearInputField();
+	};
+
+	const clearInputField = () => {
+		setAddress("");
+		setCity("");
+		setUname("");
+	};
+
+	const editUser = (e) => {
+		console.log(e.target.id);
+	};
+
+	const deleteUser = (e) => {
+		let currentUser = e.target.id;
+		console.log(currentUser);
+
+		const deleteUser = async () => {
+			let response = await axios.delete(
+				`http://localhost:3000/user/${currentUser}`
+			);
+			if (!response) {
+				throw new Error("user not deleted");
+			}
+			setFlagToGetData(true);
+		};
+		deleteUser();
 	};
 
 	return (
@@ -68,6 +140,7 @@ const Home = () => {
 								className="form-control"
 								type="text"
 								onChange={(e) => setUname(e.target.value)}
+								value={uname}
 								placeholder="USERNAME"
 							/>
 						</Col>
@@ -78,6 +151,7 @@ const Home = () => {
 								className="form-control"
 								onChange={(e) => setCity(e.target.value)}
 								type="text"
+								value={city}
 								placeholder="CITY"
 							/>
 						</Col>
@@ -88,6 +162,7 @@ const Home = () => {
 								className="form-control"
 								type="text"
 								onChange={(e) => setAddress(e.target.value)}
+								value={address}
 								placeholder="ADDRESS"
 							/>
 						</Col>
@@ -131,11 +206,19 @@ const Home = () => {
 														<td className="w-auto">{user.city}</td>
 														<td className="w-auto">{user.address}</td>
 														<td className="w-auto">
-															<Button id={user.id} className="user-btn">
+															<Button
+																id={user.id}
+																className="user-btn"
+																onClick={editUser}
+															>
 																EDIT
 															</Button>
 															<span className="px-2"></span>
-															<Button id={user.id} className="user-btn">
+															<Button
+																id={user.id}
+																className="user-btn"
+																onClick={deleteUser}
+															>
 																DELETE
 															</Button>
 														</td>
