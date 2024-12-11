f74f025aad5655fc57d3075628fe586d7a36e6ea
Saurabh Kumar <1355saurabh@gmail.com>
2024-12-12 00:23:41 +0530

    put api created with axios.

--- PRACTICE/MODULES/WebTechonolgy/day16/UserFullStack/frontend/src/pages/Home.jsx
+++ PRACTICE/MODULES/WebTechonolgy/day16/UserFullStack/frontend/src/pages/Home.jsx
@@ -1,14 +1,16 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Button, Col, Row, Table } from "react-bootstrap";
-
+import "../styles/MyLayoutStyle.css";
 const Home = () => {
 	const [data, setData] = useState([]);
+	const [id, setId] = useState(0);
 	const [uname, setUname] = useState("");
 	const [city, setCity] = useState("");
 	const [address, setAddress] = useState("");
 	// const [updateOnPost, setUpdateOnPost] = useState(false);
 	const [flagToGetData, setFlagToGetData] = useState(false);
+	const [edit, setEdit] = useState(false);
 	// useEffect(() => {
 	// 	const getUserData = async () => {
 	// 		try {
@@ -70,6 +72,9 @@ const Home = () => {
 	// };
 
 	const addUser = () => {
+		if (uname === "" || city === "" || address === "") {
+			return;
+		}
 		const newUser = { uname: uname, city: city, address: address };
 		const addUser = async () => {
 			try {
@@ -103,8 +108,50 @@ const Home = () => {
 		setUname("");
 	};
 
-	const editUser = (e) => {
-		console.log(e.target.id);
+	const goToPreviousState = () => {
+		clearInputField();
+		setEdit(false);
+	};
+
+	const editUser = async () => {
+		if (uname === "" || city === "" || address === "") {
+			setEdit(false);
+			clearInputField();
+			return;
+		}
+		const updatedUser = { id: id, uname: uname, city: city, address: address };
+		console.log(updatedUser);
+
+		let response = await axios.put(
+			`http://localhost:3000/user/${id}`,
+			updatedUser
+		);
+		if (response.status === 200 || response.status === 201) {
+			setEdit(false);
+			setFlagToGetData(true);
+			clearInputField();
+		} else {
+			throw new Error("data not updated.");
+		}
+	};
+
+	const getUserById = (e) => {
+		setEdit(true);
+		let userId = e.target.id;
+		setId(userId);
+		const getUserById = async () => {
+			let response = await axios.get(`http://localhost:3000/user/${userId}`);
+			if (response.status !== 200) throw new Error("Data not Found");
+			else {
+				let user = response.data;
+				user = user[0];
+				console.log(user);
+				setUname(user.uname);
+				setCity(user.city);
+				setAddress(user.address);
+			}
+		};
+		getUserById();
 	};
 
 	const deleteUser = (e) => {
@@ -120,12 +167,14 @@ const Home = () => {
 			}
 			setFlagToGetData(true);
 		};
+		setEdit(false);
+		clearInputField();
 		deleteUser();
 	};
 
 	return (
 		<div>
-			<Row>
+			<Row className="w-100">
 				<Col xs={6} className="pt-3">
 					<Row>
 						<Col>
@@ -142,6 +191,7 @@ const Home = () => {
 								onChange={(e) => setUname(e.target.value)}
 								value={uname}
 								placeholder="USERNAME"
+								required
 							/>
 						</Col>
 					</Row>
@@ -153,6 +203,7 @@ const Home = () => {
 								type="text"
 								value={city}
 								placeholder="CITY"
+								required
 							/>
 						</Col>
 					</Row>
@@ -164,14 +215,30 @@ const Home = () => {
 								onChange={(e) => setAddress(e.target.value)}
 								value={address}
 								placeholder="ADDRESS"
+								required
 							/>
 						</Col>
 					</Row>
 					<Row className="px-5 pt-4">
 						<Col>
-							<Button onClick={addUser} className="form-control user-btn">
-								ADD USER
-							</Button>
+							{edit === false ? (
+								<Button onClick={addUser} className="form-control user-btn">
+									ADD USER
+								</Button>
+							) : (
+								<div className="d-flex justify-content-center">
+									<Button
+										onClick={goToPreviousState}
+										className="form-control user-btn"
+									>
+										cancel
+									</Button>
+									<span className="px-2"></span>
+									<Button onClick={editUser} className="form-control user-btn">
+										save User
+									</Button>
+								</div>
+							)}
 						</Col>
 					</Row>
 				</Col>
@@ -184,55 +251,57 @@ const Home = () => {
 						</Col>
 					</Row>
 					<Row className="px-5 pt-3">
-						<Col className="">
-							<Table responsive>
-								<thead>
-									<tr className="align-center">
-										<th className="w-auto">USER ID</th>
-										<th className="w-auto">USERNAME</th>
-										<th className="w-auto">CITY</th>
-										<th className="w-auto">ADDRESS</th>
-										<th className="w-auto">ACTION</th>
-									</tr>
-								</thead>
-								<tbody>
-									{data.length > 0 ? (
-										data.map((user, idx) => {
-											{
-												return (
-													<tr className="align-center" key={idx}>
-														<td className="w-auto">{user.id}</td>
-														<td className="w-auto">{user.uname}</td>
-														<td className="w-auto">{user.city}</td>
-														<td className="w-auto">{user.address}</td>
-														<td className="w-auto">
-															<Button
-																id={user.id}
-																className="user-btn"
-																onClick={editUser}
-															>
-																EDIT
-															</Button>
-															<span className="px-2"></span>
-															<Button
-																id={user.id}
-																className="user-btn"
-																onClick={deleteUser}
-															>
-																DELETE
-															</Button>
-														</td>
-													</tr>
-												);
-											}
-										})
-									) : (
-										<tr>
-											<td colSpan="5">No Data FOUND</td>
+						<Col>
+							<div className="table-container">
+								<Table responsive>
+									<thead>
+										<tr className="align-center">
+											<th className="w-auto">USER ID</th>
+											<th className="w-auto">USERNAME</th>
+											<th className="w-auto">CITY</th>
+											<th className="w-auto">ADDRESS</th>
+											<th className="w-auto">ACTION</th>
 										</tr>
-									)}
-								</tbody>
-							</Table>
+									</thead>
+									<tbody>
+										{data.length > 0 ? (
+											data.map((user, idx) => {
+												{
+													return (
+														<tr className="align-center" key={idx}>
+															<td className="w-auto">{user.id}</td>
+															<td className="w-auto">{user.uname}</td>
+															<td className="w-auto">{user.city}</td>
+															<td className="w-auto">{user.address}</td>
+															<td className="w-auto">
+																<Button
+																	id={user.id}
+																	className="user-btn"
+																	onClick={getUserById}
+																>
+																	EDIT
+																</Button>
+																<span className="px-2"></span>
+																<Button
+																	id={user.id}
+																	className="user-btn"
+																	onClick={deleteUser}
+																>
+																	DELETE
+																</Button>
+															</td>
+														</tr>
+													);
+												}
+											})
+										) : (
+											<tr>
+												<td colSpan="5">No Data FOUND</td>
+											</tr>
+										)}
+									</tbody>
+								</Table>
+							</div>
 						</Col>
 					</Row>
 				</Col>
