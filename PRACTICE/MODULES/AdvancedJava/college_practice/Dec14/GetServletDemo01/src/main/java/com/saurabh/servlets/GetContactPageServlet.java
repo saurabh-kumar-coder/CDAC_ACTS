9559b964074a0d16a898086fdb0dc447183c98d5
Saurabh Kumar <1355Saurabh@gmail.com>
2024-12-15 07:58:48 +0530

    2 servlets running in a project.

--- PRACTICE/MODULES/AdvancedJava/college_practice/Dec14/GetServletDemo01/src/main/java/com/saurabh/servlets/GetContactPageServlet.java
+++ PRACTICE/MODULES/AdvancedJava/college_practice/Dec14/GetServletDemo01/src/main/java/com/saurabh/servlets/GetContactPageServlet.java
@@ -2,25 +2,26 @@ package com.saurabh.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Date;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-public class GetHomePageServlet extends HttpServlet {
+public class GetContactPageServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		PrintWriter out = resp.getWriter();
 		
-		out.println("<html>");
-		out.println("<body>");
-		out.println("<H1> Current Date is " + new Date() + "</H1>");
-		out.println("</body>");
-		out.println("</html>");
+		String contactUsHtml = "<html>"
+				+ "<body>"
+				+ "<h1>Contact Us</h1>"
+				+ "</body>"
+				+ "</html>";
+		
+		out.println(contactUsHtml);
 	}
 	
 }
