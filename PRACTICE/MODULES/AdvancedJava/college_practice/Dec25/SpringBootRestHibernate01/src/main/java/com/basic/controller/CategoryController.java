70f32a3fb8a742983a81f959185e811837e4f8e7
Saurabh Kumar <1355Saurabh@gmail.com>
2024-12-25 17:48:44 +0530

    category done.

--- PRACTICE/MODULES/AdvancedJava/college_practice/Dec25/SpringBootRestHibernate01/src/main/java/com/basic/controller/CategoryController.java
+++ PRACTICE/MODULES/AdvancedJava/college_practice/Dec25/SpringBootRestHibernate01/src/main/java/com/basic/controller/CategoryController.java
@@ -1,6 +1,10 @@
 package com.basic.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,9 +20,19 @@ public class CategoryController {
 	@Autowired
 	CategoryService categoryService;
 	
-	@PostMapping("/add")
+	@PostMapping()
 	public boolean addCategory(@RequestBody CategoryDTO category) {
 		return categoryService.addCategory(category);
+	}
+	
+	@GetMapping()
+	public List<CategoryDTO> findAllCategory(){
+		return categoryService.findAllCategories();
+	}
+	
+	@GetMapping("/{categoryId}")
+	public CategoryDTO findCategoryById(@PathVariable("categoryId") int categoryId) {
+		return categoryService.findByCategoryId(categoryId);
 	}
 	
 }
