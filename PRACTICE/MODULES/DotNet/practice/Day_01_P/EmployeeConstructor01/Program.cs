d456c0e057b439944062c126d5317ff275a2d2fa
Saurabh Kumar <1355Saurabh@gmail.com>
2025-01-03 07:04:51 +0530

    assignment done day 1

---
+++ PRACTICE/MODULES/DotNet/practice/Day_01_P/EmployeeConstructor01/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Dynamic;
+
+namespace employeeConstructor
+{
+    public class Employee
+    {
+        private string name;
+        public string Name { get { return name; } set { this.name = value; } }
+        private int empNo;
+        public int EmpNo
+        {
+            get { return empNo; }
+            set
+            {
+                if (value < 0)
+                {
+                    System.Console.WriteLine("wrong emp no");
+                }
+                this.empNo = value;
+            }
+        }
+        private decimal basic;
+        public decimal Basic
+        {
+            get { return basic; }
+            set { if (value < 1000) this.basic = 1000; else this.basic = value; }
+        }
+        private short deptNo;
+        public short DeptNo
+        {
+            get { return deptNo; }
+            set { if (value < 0) System.Console.WriteLine("deptno must be greated than 0"); else this.deptNo = value; }
+        }
+
+        public Employee(string Name = "ABC", int EmpNo = 1, decimal Basic = 1000, short DeptNo = 101)
+        {
+            this.Name = Name;
+            this.EmpNo = EmpNo;
+            this.Basic = Basic;
+            this.DeptNo = DeptNo;
+        }
+
+        public decimal getNetSalary()
+        {
+            return Basic + 1000;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, EmpNo: {EmpNo}, Basic: {Basic}, DeptNo: {DeptNo}, NetSalary: {getNetSalary()}";
+        }
+
+    }
+
+    public class Tester
+    {
+        public static void Main()
+        {
+            Employee employee1 = new Employee(Name: "Amol", EmpNo: 1, Basic: 123456, DeptNo: 10);
+            System.Console.WriteLine(employee1);
+            Employee employee2 = new Employee(Name: "Amol", EmpNo: 1, Basic: 123456);
+            System.Console.WriteLine(employee2);
+            Employee employee3 = new Employee(Name: "Amol", EmpNo: 1);
+            System.Console.WriteLine(employee3);
+            Employee employee4 = new Employee(EmpNo: 1);
+            System.Console.WriteLine(employee4);
+            Employee employee5 = new Employee();
+            System.Console.WriteLine(employee5);
+        }
+    }
+}
---
+++ PRACTICE/MODULES/DotNet/practice/Day_01_P/EmployeeStaticMembers/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Dynamic;
+
+namespace employeeConstructor
+{
+    public class Employee
+    {
+        private static int employeeCounter = 0;
+        private string name;
+        public string Name { get { return name; } set { this.name = value; } }
+        private int empNo;
+        public int EmpNo
+        {
+            get { return empNo; }
+            set { this.empNo = value; }
+        }
+        private decimal basic;
+        public decimal Basic
+        {
+            get { return basic; }
+            set { if (value < 1000) this.basic = 1000; else this.basic = value; }
+        }
+        private short deptNo;
+        public short DeptNo
+        {
+            get { return deptNo; }
+            set { if (value < 0) System.Console.WriteLine("deptno must be greated than 0"); else this.deptNo = value; }
+        }
+        public Employee(string Name = "ABC", decimal Basic = 1000, short DeptNo = 101)
+        {
+            this.EmpNo = ++employeeCounter;
+            this.Name = Name;
+            this.Basic = Basic;
+            this.DeptNo = DeptNo;
+        }
+
+        public decimal getNetSalary()
+        {
+            return Basic + 1000;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, EmpNo: {EmpNo}, Basic: {Basic}, DeptNo: {DeptNo}, NetSalary: {getNetSalary()}";
+        }
+
+    }
+
+    public class Tester
+    {
+        public static void Main()
+        {
+            Employee employee1 = new Employee(Name: "Amol", Basic: 123456, DeptNo: 10);
+            Employee employee2 = new Employee(Name: "Amol", Basic: 123456);
+            Employee employee3 = new Employee(Name: "Amol");
+            Employee employee4 = new Employee();
+            System.Console.WriteLine(employee1);
+            System.Console.WriteLine(employee2);
+            System.Console.WriteLine(employee3);
+            System.Console.WriteLine(employee4);
+            System.Console.WriteLine("@@@@@@@@@@ break @@@@@@@@@");
+            System.Console.WriteLine(employee4);
+            System.Console.WriteLine(employee3);
+            System.Console.WriteLine(employee2);
+            System.Console.WriteLine(employee1);
+        }
+    }
+}
