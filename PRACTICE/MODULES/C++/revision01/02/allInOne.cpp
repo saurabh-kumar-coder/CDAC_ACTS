d79f57afdeb3894d42b4d2d7e941528a3b09a5bd
Saurabh Kumar <1355Saurabh@gmail.com>
2025-01-05 03:28:56 +0530

    updated

--- PRACTICE/MODULES/C++/revision01/02/allInOne.cpp
+++ PRACTICE/MODULES/C++/revision01/02/allInOne.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<string>
 using namespace std;
 void gettingInputFromUser() {
     int a;
@@ -11,8 +12,33 @@ void addTwoString(){
     string s2 = " world";
     cout<< "\n" << s1 + s2;
 }
+void stringWithInt(){
+    // cannot use String with int, we need to typecase int to string
+    string x = "20";
+    int y = 10;
+    cout<< x + to_string(y);
+    // cout<< x.append(y);
+    // cout<< y + x;
+    // cout<< y.append(x);
+}
+void stringInput() {
+    string s1, s2;
+    cout<<"enter string1";
+    cin>>s1;
+    cout<<s1;
+    cin.ignore();
+    cout<<"\n";
+    cout<<"enter string2";
+    getline(cin, s2);
+    cout<<s2;
+}
 int main() {
-    gettingInputFromUser();
-    addTwoString();
+    // gettingInputFromUser();
+    // cout<<"\n";
+    // addTwoString();
+    // cout<<"\n";
+    // stringWithInt();
+    // cout<<"\n";
+    stringInput();
     return 0;
 }
