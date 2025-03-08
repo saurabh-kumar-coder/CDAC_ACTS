61159206d2d03fd8fefeae2b32fcc988f2c52faa
Saurabh Kumar <1355saurabh@gmail.com>
2025-03-09 04:18:59 +0530

    static method hiding

---
+++ PRACTICE/PLACEMENTS/JAVA_ECLIPSE/Basics/src/com/basic/static_hiding/Child.java
@@ -0,0 +1,7 @@
+package com.basic.static_hiding;
+
+public class Child extends Parent {
+	public static void fun() {
+		System.out.println("child");
+	}
+}
---
+++ PRACTICE/PLACEMENTS/JAVA_ECLIPSE/Basics/src/com/basic/static_hiding/Parent.java
@@ -0,0 +1,7 @@
+package com.basic.static_hiding;
+
+public class Parent {
+	public static void fun() {
+		System.out.println("parent");
+	}
+}
