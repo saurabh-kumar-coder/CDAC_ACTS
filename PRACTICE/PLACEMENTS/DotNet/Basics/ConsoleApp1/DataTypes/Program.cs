ba6598201f808a4c553fae4894c8a6376e7a3e1a
Saurabh Kumar <1355saurabh@gmail.com>
2025-03-20 02:59:06 +0530

    started .net

---
+++ PRACTICE/PLACEMENTS/DotNet/Basics/ConsoleApp1/DataTypes/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace DataTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 20;
+            Console.WriteLine(a);
+
+            char ch = 'a';
+            Console.WriteLine(ch);
+            Console.WriteLine((int)ch);
+
+            float f = 20.02f;
+            Console.WriteLine(f);
+            Console.WriteLine((int)f);
+        }
+    }
+}
