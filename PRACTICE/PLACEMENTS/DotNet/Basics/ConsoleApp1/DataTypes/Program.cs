c4dfe4b3fb9a0e75d65a32f445396130c655d204
Saurabh Kumar <1355saurabh@gmail.com>
2025-03-21 17:47:46 +0530

    TypeConversion

--- PRACTICE/PLACEMENTS/DotNet/Basics/ConsoleApp1/DataTypes/Program.cs
+++ PRACTICE/PLACEMENTS/DotNet/Basics/ConsoleApp1/DataTypes/Program.cs
@@ -2,6 +2,7 @@
 {
     internal class Program
     {
+        // Write a program to declare variables of types int, float, double, bool, char, and string, assign values, and print them.
         static void Main(string[] args)
         {
             int a = 20;
@@ -12,8 +13,17 @@
             Console.WriteLine((int)ch);
 
             float f = 20.02f;
-            Console.WriteLine(f);
-            Console.WriteLine((int)f);
+            Console.WriteLine(f);           // 20.02
+            Console.WriteLine((int)f);      // 20
+
+            double d = 10.010101013;
+            Console.WriteLine(d);
+            Console.WriteLine((float)d);
+            Console.WriteLine((int)d);
+
+            bool b = true;
+            Console.WriteLine(b);
+
         }
     }
 }
---
+++ PRACTICE/PLACEMENTS/DotNet/Basics/ConsoleApp1/TypeConversion/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace TypeConversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Convert an int to double, double to int, char to int(ASCII value), and string to int using int.Parse() and Convert.ToInt32().
+
+            int a = 23;
+            Console.WriteLine(a);
+            double d = a;
+            Console.WriteLine(d);
+
+            double dd = 23.45;
+            Console.WriteLine(dd);
+            int aa = (int)dd;
+
+        }
+    }
+}
